1a79bb3979ba315d34cd85e8710e8dc241df1bda
shubhgaur37 <shubhgaur7833@gmail.com>
2025-08-11 22:47:52 +0530

    Adding comments to App.java

--- src/App.java
+++ src/App.java
@@ -18,4 +18,14 @@ public class App {
 }
 
 
-
+/*
+Key Strengths of this approach:
+1. Simplicity: The design is kept minimal and straightforward, avoiding unnecessary complexity,
+2. Clarity: Its easy to understand, making it accessible for developers to implement and maintain.
+3. Efficiency: The implementation is direct and logical, ensuring smooth gameplay.
+4. Separation of Concerns: Each component has a clear responsibility, enhancing modularity and ease of updates.
+5. Extensibility: Support for different board sizes.
+   - More Customization Options
+   - Scalability for varied gameplay(can add AI player strategy in the future)
+   - Enhanced Flexibility
+ */
